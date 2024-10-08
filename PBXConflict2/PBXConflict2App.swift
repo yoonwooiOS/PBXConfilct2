59240eb71ef3cba703cd6c85d7e3e90dcd99d6f9
yoonwooiOS <yoonwoo4429@kakao.com>
2024-10-08 14:16:40 +0900

    initial Commit

---
+++ PBXConflict2/PBXConflict2App.swift
@@ -0,0 +1,17 @@
+//
+//  PBXConflict2App.swift
+//  PBXConflict2
+//
+//  Created by 김윤우 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PBXConflict2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
